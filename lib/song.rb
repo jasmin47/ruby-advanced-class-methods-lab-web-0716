2e935eabaf4f327b2118c00cb1cfec9164d88921
jasmin47 <alxsanborn@gmail.com>
2016-07-30 10:49:38 -0400

    all tests passed

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,62 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect {|song_object| song_object.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if @@all.include?(name) == false
+      self.create_by_name(name)
+    else self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+  @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(name)
+    song = self.new
+    song.artist_name = name.split(/ \-\ |.mp3/)[0]
+    song.name = name.split(/ \-\ |.mp3/)[1]
+    @@all << song
+    song
+  end
+
+  def self.create_from_filename(name)
+    song = self.new
+    song.artist_name = name.split(/ \-\ |.mp3/)[0]
+    song.name = name.split(/ \-\ |.mp3/)[1]
+    @@all << song
+    song
+  end
+
+
+
+  def self.destroy_all
+    self.all.clear
+  end
+
   def self.all
     @@all
   end
